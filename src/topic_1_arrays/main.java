8086f19c6da7c8e3cd7884b9da064ba7ea5d89bd
Cliffmeister <dev.clfm5t12@gmail.com>
2024-08-27 17:33:48 +0800

    Added solution 2 for arrays

--- src/topic_1_arrays/main.java
+++ src/topic_1_arrays/main.java
@@ -3,11 +3,24 @@ package topic_1_arrays;
 public class main {
 
 	public static void main(String[] args) {
-
+		
 		String[] person = {"Cliff Amadeus","27", "Instructor"};
+		
+		//Solution 1
+		System.out.println("Solution 1:");
+		System.out.println("----------------------");
 		System.out.println(person[0]);
 		System.out.println(person[1]);
 		System.out.println(person[2]);
+		
+		System.out.println("");
+	
+		//Solution 2
+		System.out.println("Solution 2:");
+		System.out.println("----------------------");
+		for(int i=0; i< person.length; i++) {
+			System.out.println(person[i]);
+		}
 
 	}
 
