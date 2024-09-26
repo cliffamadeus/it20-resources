e6339003c0a2a3e2fed2043805512582995820b8
Cliffmeister <dev.clfm5t12@gmail.com>
2024-09-26 20:55:45 +0800

    Added node class for linkedlist-string

---
+++ src/topic_2_linkedlist_string/LinkedList.java
@@ -0,0 +1,5 @@
+package topic_2_linkedlist_string;
+
+public class LinkedList {
+
+}
---
+++ src/topic_2_linkedlist_string/Main.java
@@ -0,0 +1,10 @@
+package topic_2_linkedlist_string;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
