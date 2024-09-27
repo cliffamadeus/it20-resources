e9d12de1107b949f93f5c7842f8f404abaed5a2f
Cliffmeister <dev.clfm5t12@gmail.com>
2024-09-27 12:48:07 +0800

    Added add method on linkedlist_dating

---
+++ src/topic_2_linkedlist_dating/LinkedList.java
@@ -0,0 +1,17 @@
+package topic_2_linkedlist_dating;
+
+public class LinkedList {
+	  private Node head;
+	  private Node tail;
+
+	  public void add(String data) {
+	      Node newNode = new Node(data);
+	        if (head == null) {
+	            head = newNode;
+	            tail = newNode;
+	        } else {
+	            tail.next = newNode;
+	            tail = newNode;
+	        }
+	   }
+}
