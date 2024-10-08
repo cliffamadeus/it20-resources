9e39a479a7150d6cdcb5f5846ab2d44b3a6a5027
cliffamadeus <reconcliffy@gmail.com>
2024-10-08 14:00:45 +0800

    Added pop method and added use case on main

--- src/topic_3_stack/Main.java
+++ src/topic_3_stack/Main.java
@@ -11,6 +11,12 @@ public class Main {
         
         System.out.println("Stack size is: " + stack.size());
         System.out.println("Top element is: " + stack.peek());
+        System.out.println();
+        
+        //Use case
+        System.out.println("Popped element: " + stack.pop());
+        System.out.println("Top element after pop: " + stack.peek());
+        System.out.println("Stack size after pop: " + stack.size());
 
 	}
 
