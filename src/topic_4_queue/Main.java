8b42b63e47c18ba8cfd6a8086775e3a1175ca685
cliffamadeus <reconcliffy@gmail.com>
2024-10-11 08:53:23 +0800

    added queue resources

---
+++ src/topic_4_queue/Main.java
@@ -0,0 +1,10 @@
+package topic_4_queue;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/topic_4_queue/Queue.java
@@ -0,0 +1,7 @@
+package topic_4_queue;
+import java.util.LinkedList;
+
+public class Queue {
+	
+
+}
